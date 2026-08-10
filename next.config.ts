1baa72e311a9a8d07df424ba853ccc3d3d4be4be
YoungjunRyoo <yjblack99@gmail.com>
2026-08-10 11:56:14 -0500

    next.config.ts 변경

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,19 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "https",
+        hostname: "feqtuftcleljtbgxkrsv.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
       },
     ],
   },
